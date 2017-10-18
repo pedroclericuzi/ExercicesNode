b4271e2a17ee58c1dc76e48220f393825ce0a200
pedroclericuzi <pvbc@cin.ufpe.br>
2017-10-18 03:44:38 -0300

    questao 2.4

--- E2 - COAP/coap-descovery.js
+++ E2 - COAP/coap-descovery.js
@@ -2,35 +2,46 @@ var coap = require('../../wot-book-master/chapter7-implementation/part1-2-direct
   utils = require('../../wot-book-master/chapter7-implementation/part1-2-direct-gateway/utils/utils');
 
 var port = 5683;
+function randomInt (low, high) {
+  return Math.floor(Math.random() * (high - low) + low);
+}
 
 coap.createServer(function (req, res) {
   console.info('CoAP device got a request for %s', req.url);
-
-  if (req.headers['Accept'] != 'application/json') {
-    res.setHeader('Content-Type', req.headers['Accept']);
-  } else {
-    res.setHeader('Content-Type', 'application/json');
-  } 
+  console.info('CoAP device got a request for %s', req.headers['Accept']);
 
   switch (req.url) {
     case "/co2":
-      respond(res, {'co2': utils.randomInt(0, 1000)}); 
+      respond(req, res, '{"co2" :' + randomInt(1, 40) + '}'); 
       break;
     case "/temp":
-      respond(res, {'temp': utils.randomInt(0, 40)});
+      respond(req, res, '{"temperature" :' + randomInt(1, 40) + '}');
       break;
     default:
-      respond(res);
+      respond(req, res);
   }
 }).listen(port, function () {
   console.log("CoAP server started on port %s", port)
 });
 
-function respond(res, content) { 
+function respond(req, res, content) { 
   if (content) {
-    res.setOption('Content-Format', 'application/json');
-    res.code = '2.05';
-    res.end(JSON.stringify(content));
+
+    switch (req.headers['Accept']) {
+      case "application/xml":
+        res.setOption('Content-Format', ''+req.headers['Accept']+'');
+        break;
+      case "text/html":
+        res.setOption('Content-Format', ''+req.headers['Accept']+'');
+        break;
+      case "text/plain":
+        res.setOption('Content-Format', ''+req.headers['Accept']+'');
+        break;
+      default:
+        res.setOption('Content-Format', 'application/json');
+    }
+    //res.code = '2.05';
+    res.end(content);
   } else {
     res.code = '4.04';
     res.end();
